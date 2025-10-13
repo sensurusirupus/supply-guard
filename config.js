0d5e1ccc855d86a69d792c5e829650b97a7105b7
sensurusirupus <sensurusirupus@gmail.com>
2025-10-13 14:36:05 +0000

    style(config): LUpdate application configuration

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
+// Defines the smart contract address for the SupplyGuard feature.
 export const supplyGuardAddress = "0x9eB5D7142Dcb86c6d25EFFe13afDDE15f59518E4";
 // Last updated: Tue Sep 23 19:46:01 GMT 2025
