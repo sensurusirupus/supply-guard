71da86d40b3091239722551788657fe1a19ab61a
sensurusirupus <sensurusirupus@gmail.com>
2025-10-30 16:27:24 +0000

    Retain hardhat network (chainId 296) for local dev

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,30 +8,10 @@ module.exports = {
     hardhat: {
       chainId: 296,
     },
-    sepolia: {
-      url: "https://rpc.sepolia-api.lisk.com",
-      accounts: [privateKey],
-      gasPrice: 1000000000,
-    },
     HederaTestnet: {
       url: "https://testnet.hashio.io/api",
       accounts: [privateKey],
       gasPrice: 1000000000,
-    },
-    mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [privateKey],
-    },
-
-    matic: {
-      url: "https://polygon-mumbai.g.alchemy.com/v2/2bGIFu-iEnl9RvAOTe1ddZI2gBnuYQGS",
-      accounts: [privateKey],
-    },
-    kovan: {
-      url: "https://kovan.infura.io/v3/745fcbe1f649402c9063fa946fdbb84c",
-      accounts: [privateKey],
-      gas: 2100000,
-      gasPrice: 8000000000,
     },
   },
   solidity: "0.8.24",
